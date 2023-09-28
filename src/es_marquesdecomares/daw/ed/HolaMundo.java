8cb17623a263ad4e23f7d62b9000f64c0eb487e4
arroyosergi3 <sergioaj815@gmail.com>
2023-09-28 11:27:32 +0200

    fix(comments): New LInes

--- src/es_marquesdecomares/daw/ed/HolaMundo.java
+++ src/es_marquesdecomares/daw/ed/HolaMundo.java
@@ -3,8 +3,8 @@ package es_marquesdecomares.daw.ed;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-		System.out.println("Hola mundo 2");
+		System.out.println("Soy Alejandro Esteban");
+		System.out.println("Soy Sergio Arroyo");
 	}
 
 }
