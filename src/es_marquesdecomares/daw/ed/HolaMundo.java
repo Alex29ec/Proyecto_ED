4b757513b0944f2df75e26fe131e4754b1541d7d
Alex29ec <alejandroestebancantero@gmail.com>
2023-10-03 09:09:09 +0200

    feat():new line add

--- src/es_marquesdecomares/daw/ed/HolaMundo.java
+++ src/es_marquesdecomares/daw/ed/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Soy Alejandro Esteban");
 		System.out.println("Soy Sergio Arroyo");
+		System.out.println("WEnas");
 	}
-
+	
 }
